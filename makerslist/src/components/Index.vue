6e65499b2925f7923357303a0be5c3a9c5c4c7a0
unknown <orthogonaltrajectory@gmail.com>
2019-01-27 14:02:52 -0900

    Cards for displaying data from dummy lists

---
+++ makerslist/src/components/Index.vue
@@ -0,0 +1,53 @@
+<template>
+<!-- container makes a central column on the page so text doesn't hug the browser edges -->
+  <div class="index container">
+    <div class="card orange" v-for="list in lists" :key="list.id">
+      <div class="card-content">
+        <h2 class="white-text">{{list.title}}</h2>
+        <ul class="items">
+          <li v-for="(item,index) in list.items" :key="index"><span class="chip">{{item}}</span></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  data () {
+    return {
+      lists: [
+        // slug: is a URL friendly version of the title.
+        {title: 'Drone Parts', slug: 'drone-parts', items: ['carbon fiber frame', 'motors', 'props', 'cc3d', 'pdb'], id:'1'},
+        {title: 'Robot Parts', slug: 'robot-parts', items: ['plexiglass', 'motors', 'raspberry pi'], id:'2'},
+      ]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+.index {
+  /* display items side by side with three items per row */
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-gap: 30px;
+  margin-top: 60px;
+}
+
+.index h2 {
+  font-size: 2em;
+  text-align: center;
+  margin-top: 0px;
+}
+
+.index .items {
+  margin: 30px auto;
+}
+
+.index .items li {
+  display: inline-block;
+}
+</style>
---
+++ makerslist/src/components/Navbar.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="navbar">
+        <!-- Position nav at top of page -->
+        <nav class="nav-extended indigo darken-2">
+            <div class="nav-content">
+                <router-link to="">
+                    <span class="nav-title">MakersList</span>
+                </router-link>
+                <!-- halfway-fab pushes button halfway beyond nav boundary -->
+                <a href="" class="btn-floating btn-large halfway-fab orange">
+                    <router-link to="">
+                        <!-- <i> for icon "add" is the name of the icon -->
+                        <i class="material-icons">add</i>
+                    </router-link>
+                </a>
+            </div>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Navbar',
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style>
+.navbar nav {
+    padding: 0 20px;
+}
+</style>

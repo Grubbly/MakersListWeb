59ed2ac1b1b84d82c9c98125b17fab7cef98ed9d
orthogonaltrajectory@gmail.com <orthogonaltrajectory@gmail.com>
2019-02-11 22:25:08 -0900

    Signup, login, viewprofile templates

---
+++ makerslist/src/components/profile/ViewProfile.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="view-profile container">
+        <div v-if="profile" class="card">
+            <h2 class="cyan-text text-darken-4 center">{{profile.alias}}'s Profile</h2>
+        </div>
+    </div>
+</template>
+
+<script>
+import db from '@/firebase/init'
+
+export default {
+    name: 'ViewProfile',
+    data() {
+        return{
+            profile: null
+        }
+    },
+    created() {
+        let ref = db.collection('users')
+        ref.doc(this.$route.params.id).get()
+        .then(user => {
+            this.profile = user.data()
+        })
+    }
+}
+</script>
+
+<style>
+.view-profile {
+    max-width: 600px;
+    margin-top: 60px;
+}
+
+.view-profile h2 {
+    font-size: 2.4em;
+}
+</style>
+
+
--- makerslist/src/router/index.js
+++ makerslist/src/router/index.js
@@ -3,10 +3,14 @@ import Router from 'vue-router'
 import Index from '@/components/Index'
 import AddList from '@/components/AddList'
 import EditList from '@/components/EditList'
+import Signup from '@/components/auth/Signup'
+import Login from '@/components/auth/Login'
+import ViewProfile from '@/components/profile/ViewProfile'
+import firebase from 'firebase'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   routes: [
     {
@@ -23,6 +27,44 @@ export default new Router({
       path: '/edit-list/:list_slug',
       name: 'EditList',
       component: EditList
+    },
+    {
+      path: '/signup',
+      name: 'Signup',
+      component: Signup
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/profile/:id',
+      name: 'ViewProfile',
+      component: ViewProfile,
+      meta: {
+        requiresAuth: true
+      }
     }
   ]
 })
+
+router.beforeEach((to,from,next) => {
+  // to = destination
+  // from = current location
+  // next = function used to say where to go next
+  // Check if auth is req for route
+  if(to.matched.some(rec => rec.meta.requiresAuth)) {
+    let user = firebase.auth().currentUser
+    if(user) {
+      // user sign in, go to route
+      next()
+    } else {
+      next({name: 'Login'})
+    }
+  } else {
+    next()
+  }
+})
+
+export default router

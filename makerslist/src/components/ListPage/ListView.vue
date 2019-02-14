b8e7be978f8ba9464bc4e255b7f907d29e4d2360
unknown <orthogonaltrajectory@gmail.com>
2019-02-14 11:47:38 -0900

    Materialize card reveal

--- makerslist/src/components/ListPage/ListView.vue
+++ makerslist/src/components/ListPage/ListView.vue
@@ -37,21 +37,22 @@
             <v-card>
                 <v-container grid-list-xl fluid grey lighten-3>
                 <v-layout row wrap>
-                    <v-flex v-for="n in 8" :key="n" xs3 d-flex>
-                    <v-card flat tile class="d-flex">
-                        <v-img
-                        :src="`https://picsum.photos/500/300?image=${Math.floor(n * (Math.random()*100) + 50)}`"
-                        :lazy-src="`https://picsum.photos/10/6?image=${Math.floor(n * (Math.random()*100) + 50)}`"
-                        aspect-ratio="1"
-                        class="cyan darken-4"
-                        >
-                        <v-layout slot="placeholder" fill-height align-center justify-center ma-0>
-                            <v-progress-circular indeterminate color="cyan"></v-progress-circular>
-                        </v-layout>
-                        </v-img>
-                    </v-card>
+                    <v-flex v-for="(item,index) in list.items" :key="index" xs3 d-flex>
+                          <v-card class="card">
+                            <div class="card-image">
+                                <img class="activator" :src="`https://picsum.photos/500/300?image=${Math.floor(n * (Math.random()*30))}`">
+                            </div>
+                            <div class="card-content">
+                                <span class="card-title activator cyan-text text-darken-4">{{item}}<i class="material-icons right">more_vert</i></span>
+                            <p><a href="#">Buy it now</a></p>
+                            </div>
+                            <div class="card-reveal">
+                                <span class="card-title cyan-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+                                <p>Here is some more information about this product that is only revealed once clicked on.</p>
+                            </div>
+                        </v-card>
                     </v-flex>
-                </v-layout>
+                    </v-layout>
                 </v-container>
             </v-card>
         </v-flex>

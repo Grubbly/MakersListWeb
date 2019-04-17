f531e3758b38ce15c3f100464b889f023d50783c
Ryan Stonebraker <ryanastonebraker@gmail.com>
2019-04-16 17:30:16 -0800

    Fixes favorite button

--- makerslist/src/components/Index.vue
+++ makerslist/src/components/Index.vue
@@ -43,10 +43,12 @@
               style="position: relative;"
             >
               <RadialButton class="edit" v-on:delete="deleteList($event)" :list="list"/>
-              <router-link :to="{name: 'ListView', params: {list_slug: list.slug}}">
-                <h3 class="display-1 font-weight-light orange--text mb-2">{{list.title}}<Favorite v-on:toggleFav="handleListFavorite($event)" :list="list"/></h3>
-                <h4 class="font-weight-light grey--text title mb-2">Quick Description</h4>
-                <h6 class="green-text text-lighten-2">${{list.total}}</h6>
+              <div class="title">
+                <router-link :to="{name: 'ListView', params: {list_slug: list.slug}}"><h3 class="display-1 font-weight-light orange--text mb-2">{{list.title}}</h3></router-link>
+                <Favorite v-on:toggleFav="handleListFavorite($event)" :list="list"/>
+              </div>
+              <h4 class="font-weight-light grey--text title mb-2">Quick Description</h4>
+              <h6 class="green-text text-lighten-2">${{list.total}}</h6>
               </router-link>
                 <v-expansion-panel expand>
                   <v-expansion-panel-content class="grey lighten-2">

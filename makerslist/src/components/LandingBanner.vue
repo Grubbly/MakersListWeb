dee5c38a52bad273b9da9fb1419f623a34bfbe87
Ryan Stonebraker <ryanastonebraker@gmail.com>
2019-04-25 11:52:26 -0800

    Adds landing banner component

---
+++ makerslist/src/components/LandingBanner.vue
@@ -0,0 +1,37 @@
+<template>
+  <section class="landingBanner">
+    <v-carousel
+      height="340px"
+    >
+    <v-carousel-item
+      v-for="(item,i) in getLandingBanners()"
+      :key="i"
+      :src="item"
+    >
+    </v-carousel-item>
+   </v-carousel>
+  </section>
+</template>
+
+
+<script>
+import db from '@/firebase/init'
+
+export default {
+  name: 'LandingBanner',
+  components: {
+  },
+  props: {
+      list: null,
+  },
+  data () {
+    return {
+    }
+  },
+  methods: {
+    getLandingBanners(index) {
+      return ["/static/img/logo.svg", "/static/img/logo.svg", "/static/img/logo.svg"]
+    }
+  },
+}
+</script>
